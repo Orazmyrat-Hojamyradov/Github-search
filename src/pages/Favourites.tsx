7655085b4a8c223e45b41c4b8f9df3ad02ead01f
Orazmyrat-Hojamyradov <yakupyakupbow197@gmail.com>
2024-10-09 17:53:48 +0500

    deploy

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -19,21 +19,21 @@ export default function Favourites() {
       {favs.length > 0 ? (
         <ul className="fav-list">
           {favs.map((username) => (
-            <>
+            <div className="fav-box" key={username}>
               <a className="fav-link" href={`https://github.com/${username}`}>
                 <li className="fav-list-user" key={username}>
                   {username}
                 </li>
               </a>
-              <a href={`https://github.com/${username}?tab=repositories`}>
+              {/* <a href={`https://github.com/${username}?tab=repositories`}>
                 <button className="repo-link-btn">Go to repos</button>
-              </a>
+              </a> */}
 
               <FaHeart
                 className="fav"
                 onClick={() => handleRemoveFromFavs(username)}
               />
-            </>
+            </div>
           ))}
         </ul>
       ) : (
